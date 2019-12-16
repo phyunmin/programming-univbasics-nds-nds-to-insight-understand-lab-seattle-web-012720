2454e8a97e504573216d32303d6b05592b63b7d2
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-16 06:40:19 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,15 +11,5 @@ end
 
 def print_first_directors_movie_titles
   pp directors_database
-  row_index = 0
-  while row_index < directors_database.length do
-    if(directors_database[row_index][:name] == "Stephen Spielberg")
-      col_index = 0
-      while col_index < directors_database[row_index][:movie][col_index].length do
-#        puts "#{directors_database[row_index][:movie][col_index]}"
-      end
-      row_index+=1
-    end
-    row_index+=1
-  end
+  
 end
